dd6d44f51441b36fc3bbf0a97d9893273609e124
adrien <2876425803@qq.com>
2018-05-25 13:19:36 +0800

    更新

--- phase1/act_101/js/index_ed06.js
+++ phase1/act_101/js/index_ed06.js
@@ -214,11 +214,22 @@ function pushHistory(){
 }
 
 function pushBack(url){
-    window.history.pushState(1, '', '');
+    if(getParam('backdraw') !== '0'){
+        window.history.pushState(1, '', '');
 
-    window.addEventListener("popstate", function(e) {
-        location.href = url;
-    }, false);
+        window.addEventListener("popstate", function(e) {
+            location.href = url;
+        }, false);
+    }
+}
+
+function getParam(paramName) {
+    paramValue = "", isFound = !1;
+    if (this.location.search.indexOf("?") == 0 && this.location.search.indexOf("=") > 1) {
+        arrSource = unescape(this.location.search).substring(1, this.location.search.length).split("&"), i = 0;
+        while (i < arrSource.length && !isFound) arrSource[i].indexOf("=") > 0 && arrSource[i].split("=")[0].toLowerCase() == paramName.toLowerCase() && (paramValue = arrSource[i].split("=")[1], isFound = !0), i++
+    }
+    return paramValue == "" && (paramValue = null), paramValue
 }
 
 $(function(){
