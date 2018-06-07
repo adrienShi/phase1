f8fe60b4f64414073f5ba8a445dab0c64fd7f571
adrien <2876425803@qq.com>
2018-06-07 10:42:05 +0800

    更新

--- phase1/act_104/js/list_ed04.js
+++ phase1/act_104/js/list_ed04.js
@@ -70,6 +70,15 @@ $(function(){
         console.log("complete");
 	});
 
+	function getParam(paramName) {
+		paramValue = "", isFound = !1;
+		if (this.location.search.indexOf("?") == 0 && this.location.search.indexOf("=") > 1) {
+			arrSource = unescape(this.location.search).substring(1, this.location.search.length).split("&"), i = 0;
+			while (i < arrSource.length && !isFound) arrSource[i].indexOf("=") > 0 && arrSource[i].split("=")[0].toLowerCase() == paramName.toLowerCase() && (paramValue = arrSource[i].split("=")[1], isFound = !0), i++
+		}
+		return paramValue == "" && (paramValue = null), paramValue
+	}
+
 	function pushBack(url){
 		if(getParam('backdraw') !== '0'){
 			window.history.pushState(1, '', '');
